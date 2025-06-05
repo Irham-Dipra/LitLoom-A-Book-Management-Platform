9dc4b4d3d737069b5142706494870c58ba65d202
dipra <diprairham3223@gmail.com>
2025-06-05 18:02:42 +0600

    added basic api-endpoints

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,11 @@
+const pool = require('../db');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+};
