81f0ba2385195b18c981b70e8051c328dbba67f8
Dipra <diprairham3223@gmail.com>
2025-08-31 06:08:43 +0600

    adding db

---
+++ backend/db/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'db.vptaxhbczlifinecnygf.supabase.co',
+  database: process.env.DB_NAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'litloom3223',
+  port: process.env.DB_PORT || 5432,
+});
+
+module.exports = pool;
