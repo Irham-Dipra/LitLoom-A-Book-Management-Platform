9569a227584c18c2d9776fe41fa5fd1f34961c11
Dipra <diprairham3223@gmail.com>
2025-08-31 06:43:58 +0600

    adding db

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,14 +2,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'db.vptaxhbczlifinecnygf.supabase.co',
-  database: process.env.DB_NAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'litloom3223',
-  port: process.env.DB_PORT || 5432,
-  ssl: {
-    rejectUnauthorized: false, // required for Supabase
-  },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false } // Supabase needs SSL
 });
+
 
 module.exports = pool;
